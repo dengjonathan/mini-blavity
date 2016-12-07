c2c3ea540fce035cc01a05358dd570f178d50799
jonathan.deng <dengjonathan@gmail.com>
2016-12-07 18:26:19 -0500

    feat (api): add endpoint to get one machine from sku

--- app/utils/handlers.js
+++ app/utils/handlers.js
@@ -5,7 +5,18 @@ exports.getAllMachines = (req, res) => {
   machines.fetch().then(data => res.send(data));
 };
 
-exports.getOneMachine = (req, res) => {};
+exports.getOneMachine = (req, res) => {
+  console.log(req.params.id);
+  machines.query({where: {sku: req.params.id}})
+    .fetchOne()
+    .then(machine => {
+      console.log(machine);
+      if (!machine) {
+        return res.send('that query not found');
+      }
+      return res.send(machine);
+    });
+};
 
 exports.getAllPods = (req, res) => {};
 
