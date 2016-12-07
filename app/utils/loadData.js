8769aa055522ae0fe79b0348f2d123f54a685385
jonathan.deng <dengjonathan@gmail.com>
2016-12-07 18:02:16 -0500

    feat (db): load initial data

---
+++ app/utils/loadData.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const Machines = require('../collections/machines');
+const Pods = require('../collections/pods');
+
+const readJSON = cb => {
+  fs.readFile(path.join(__dirname, '/initial_data.json'), 'utf8', (err, text) => {
+    if (err) {
+      throw err;
+    }
+    cb(JSON.parse(text));
+  });
+};
+
+exports.loadMachines = () => readJSON(data => {
+  data.machines.forEach(machine => Machines.create(machine));
+});
+
+exports.loadPods = () => readJSON(data => {
+  data.pods.forEach(pod => Pods.create(pod));
+});
